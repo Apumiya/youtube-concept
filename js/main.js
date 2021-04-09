bb5695b3c4a7c90db773d9193e4f7322015bfd33
Юлия <61793004+Apumiya@users.noreply.github.com>
2021-04-09 12:28:27 +0300

    Пофиксили меню и шапку, добавили контент

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 5,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.chanel-button-next',
+    prevEl: '.chanel-button-prev',
+  },
+});
